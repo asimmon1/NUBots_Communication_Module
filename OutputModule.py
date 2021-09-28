334e1417ff9774991162834fd6c1c785d1d680c5
Angus Simmons <asimmon1125@gmail.com>
2021-09-28 21:48:46 +1000

    can now format instructions properly

--- OutputModule.py
+++ OutputModule.py
@@ -8,7 +8,7 @@ def tester():
         # verbally_respond(intention_list, "find_name_in_place")
 
         testEntities =[{'entity': '"name"', 'start': 5, 'end': 10, 'confidence_entity': 0.8220005035400391, 'value': 'Julia', 'extractor': 'DIETClassifier'}, {'entity': '"place"', 'start': 18, 'end': 23, 'confidence_entity': 0.8015493750572205, 'value': 'bench', 'extractor': 'DIETClassifier'}]
-        format_Instructions(intention_list["find_name_in_place"]["Instruction_set"], testEntities)
+        print(format_Instructions(intention_list["find_name_in_place"]["Instruction_set"], testEntities))
         # print(fruits_list["Intention_name"]["Instruction_set"])
         # x = 0
         # for instruction in fruits_list["Intention_name2"]["Instruction_set"]:
@@ -29,26 +29,29 @@ def format_Instructions(intructions_set, entities):
 
     single_instructions = intructions_set.split(" -")
     single_instructions[0] = single_instructions[0].replace("-", "")
-    for string in single_instructions:
-        fill_entity(string, entities)
+    new_instruction_list = []
+    for i in range(len(single_instructions)):
+        single_instructions[i] = fill_entity(single_instructions[i], entities)
+        formatted_instruction = str(i+1)+": "
+        for string in single_instructions[i]:
+            formatted_instruction += string+" "
 
+        new_instruction_list += [formatted_instruction]
+
+    return new_instruction_list
 def fill_entity(string, entities):
 
     words = string.split(" ")
     for i in range(len(words)):
         if "[" in words[i]:
-            print(words[i])
             words[i] = words[i].strip("[")
             words[i] = words[i].strip("]")
             for entity in entities:
-                print(entity["entity"])
-                print(entity["value"])
                 if entity["entity"].lower().strip("\"") == words[i]:
-                    print("ding ding")
                     words[i] = entity["value"]
 
 
-    print(words)
+    return(words)
 
 def extract_instructions(heading):
     print("this will extract the instructions for the intention under the heading")
